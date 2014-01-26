52d291061da15cf10d7daea18215ecc698f14ee1
Steve Kinney <hello@stevekinney.net>
2014-01-26 17:29:41 -0500

    Set up server foundation

---
+++ index.js
@@ -0,0 +1,18 @@
+var restify = require('restify');
+var server = restify.createServer();
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.authorizationParser());
+
+server.get('/', function (req, res, next) {
+  res.send(200, {status: 'ok'});
+});
+
+server.post('/messages', function (req, res, next) {
+  res.send(200, {status: 'ok'});
+});
+
+server.listen(process.env.PORT || '3000', function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
