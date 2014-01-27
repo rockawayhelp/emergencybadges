f64e909be84b41b8c1d890cef81f11f159fb3659
Steve Kinney <hello@stevekinney.net>
2014-01-27 06:09:41 -0500

    Add some additional linting

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -11,15 +11,17 @@ module.exports = function (server) {
       database.flushall(function () {
         messenger.send(phoneNumber, 'The database has been flushed.');
       });
-      res.send(200);
+      res.send(200, {status: 'The database has been flushed.'});
       return;
     }
   
     database.get(phoneNumber, function(err, user) {
       if (err) {
         messenger.fail(phoneNumber);
-        res.send(500);
+        res.send(500, {error: err});
+        return;
       }
+      
       if (user) {
         messenger.send(phoneNumber, 'Hello, old friend. (' + user + ')' );
       } else {
@@ -27,9 +29,9 @@ module.exports = function (server) {
           messenger.send(phoneNumber, 'Nice to meet you.');
         });
       }
+      
+      res.send(200, {status: 'Recieved', message: message});
     });
-  
-    res.send(200, {status: 'ok'});
   });
   
 };
