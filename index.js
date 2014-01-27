c380459c90a70a345b07ef963534d76fbac42f5d
Steve Kinney <hello@stevekinney.net>
2014-01-26 19:22:47 -0500

    Store phone numbers in memory and respond differently

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var messenger = require('./lib/messenger');
 
 var server = restify.createServer();
 
+var users = {};
+
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 server.use(restify.authorizationParser());
@@ -13,7 +15,14 @@ server.get('/', function (req, res, next) {
 
 server.post('/messages', function (req, res, next) {
   var phoneNumber = req.params.From;
-  messenger.send(phoneNumber, 'Got it.');
+  
+  if users[phoneNumber] {
+    messenger.send(phoneNumber, 'Hello, old friend.');
+  } else {
+    messenger.send(phoneNumber, 'Nice to meet you.');
+    users[phoneNumber] = req.params;
+  }
+  
   res.send(200, {status: 'ok'});
 });
 
