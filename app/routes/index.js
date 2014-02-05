06b1127160f0c8968c893eaac3094facdeb2cffc
Steve Kinney <hello@stevekinney.net>
2014-02-05 14:20:58 -0500

    Add index of routes

---
+++ app/routes/index.js
@@ -0,0 +1,13 @@
+var applyMessageRoutes = require('./messages');
+var applyTaskRoutes = require('./tasks');
+
+module.exports = function (server) {
+  
+  server.get('/', function (req, res, next) {
+    res.send(200, {name: 'Emergency Badges', status: 'ok'});
+  });
+  
+  applyMessageRoutes(server);
+  applyTaskRoutes(server);
+  
+}
