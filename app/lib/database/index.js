517554f05446ecc9f9c3dd850293cdeb5b5a87ea
Steve Kinney <hello@stevekinney.net>
2014-02-10 10:35:31 -0500

    Change order of Couch locations

--- app/lib/database/index.js
+++ app/lib/database/index.js
@@ -1,7 +1,7 @@
 var cradle = require('cradle');
 var applyDesignDocuments = require('./design-documents');
 
-var couchLocation = process.env.COUCHDB || process.env.CLOUDANT_URL;
+var couchLocation = process.env.CLOUDANT_URL || process.env.COUCHDB || 'http://localhost';
 var couch = new(cradle.Connection)(couchLocation, 5984, {
   cache: true,
   raw: false
