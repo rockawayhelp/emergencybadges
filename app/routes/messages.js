db189cbc015f2a68b4a2a55d59b845f04dc915de
Steve Kinney <hello@stevekinney.net>
2014-02-17 16:06:48 -0500

    Log errors on messages received to the console

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -1,6 +1,5 @@
 var User = require('../lib/models/users');
 var tasks = require('../lib/models/tasks');
-var _ = require('lodash');
 
 module.exports = function (server) {
   
@@ -9,7 +8,7 @@ module.exports = function (server) {
     var message = req.params.Body;
     
     User.findOrCreate(phoneNumber, function (err, user) {
-      if (err) { res.send(500, err); return; }
+      if (err) { console.log(err); return res.send(500, err); }
       user.respondToMessage(message, res);
     });
   });
